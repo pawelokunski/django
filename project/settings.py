105d2c0d7bfbe0799964c594010819591aeb343d
pawelokunski <pawel.okunski@wp.pl>
2024-05-15 15:08:07 +0200

    commit

--- project/settings.py
+++ project/settings.py
@@ -130,11 +130,15 @@ LOCALE_PATHS = [
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+
+STATIC_URL = '/static/'
 if DEBUG:
     STATICFILES_DIRS = [BASE_DIR / 'static']
 else:
     STATIC_ROOT = BASE_DIR / 'static'
-# STATIC_URL = '/static/'
+
+# else:
+
 
 MEDIA_ROOT  = BASE_DIR / 'media'
 MEDIA_URL = '/media/'
